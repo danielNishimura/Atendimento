36fa1b3e2d7f2b6b5474f574ace4a9219e7a79a7
Daniel Nishimura <71443012+danielNishimura@users.noreply.github.com>
2021-05-30 19:35:11 -0300

    Add arquivos no repositorio
    Add arquivos no repositorio

---
+++ dados.php
@@ -0,0 +1,39 @@
+<?php
+
+    $cnpj = $_GET["cCnpj"];
+    $razaoSocial = $_GET["cRazaoSocial"];
+    $fantasia = $_GET["cFantasia"];
+    $funcionario = $_GET["cFuncionario"];
+    $cpf = $_GET["cCpf"];
+    $dataNascimento = $_GET["cDataNascimento"];
+    $cargo = $_GET["cCargo"];
+    $setor = $_GET["cSetor"];
+    $riscoFisico = $_GET["cRiscoFisico"];
+    $riscoQuimico = $_GET["cRiscoQuimico"];
+    $riscoBiologico = $_GET["cRiscoBiologico"];
+    $riscoErgonomico = $_GET["cRiscoErgonomico"];
+    $riscoAcidente = $_GET["cRiscoAcidente"];
+    $medicoCoordenador = $_GET["cMedicocoordenador"];
+    $observacao = $_GET["cObservacao"];
+    $convenio = $_GET["cConvenio"];
+
+    $exame1 = $_GET["cExame1"];
+    $exame2 = $_GET["cExame2"];
+    $exame3 = $_GET["cExame3"];
+    $exame4 = $_GET["cExame4"];
+    $exame5 = $_GET["cExame5"];
+    $exame6 = $_GET["cExame6"];
+    $exame7 = $_GET["cExame7"];
+    $exame8 = $_GET["cExame8"];
+
+    $data1 = $_GET["cData1"];
+    $data2 = $_GET["cData2"];
+    $data3 = $_GET["cData3"];
+    $data4 = $_GET["cData4"];
+    $data5 = $_GET["cData5"];
+    $data6 = $_GET["cData6"];
+    $data7 = $_GET["cData7"];
+    $data8 = $_GET["cData8"];
+
+    echo "CNPJ: ".$cnpj."<br/>Razao Social: ".$razaoSocial."<br/>Nome Fantasia: ".$fantasia."<br/>Nome do funcionario: ".$funcionario."<br/>Numero do CPF: ".$cpf."<br/>Data de Nascimento: ".$dataNascimento."<br/>Cargo: ".$cargo."<br/>Setor: ".$setor."<br/>Risco Fisico: ".$riscoFisico."<br/>Risco Quimico: ".$riscoQuimico."<br/>Risco Biologico: ".$riscoBiologico."<br/>Risco Ergonomico: ".$riscoErgonomico."<br/>Risco Acidente: ".$riscoAcidente."<br/>Medico Coordenador: ".$medicoCoordenador."<br/>Observação: ".$observacao."<br/>Convenio: ".$convenio."<br/>Exame1: ".$exame1."<br/>Exame2: ".$exame2."<br/>Exame3: ".$exame3."<br/>Exame4: ".$exame4."<br/>Exame5: ".$exame5."<br/>Exame6: ".$exame6."<br/>Exame7: ".$exame7."<br/>Exame8: ".$exame8."<br/>Data Exame1: ".$data1."<br/>Data Exame2: ".$data2."<br/>Data Exame3: ".$data3."<br/>Data Exame4: ".$data4."<br/>Data Exame5: ".$data5."<br/>Data Exame6: ".$data6."<br/>Data Exame7: ".$data7."<br/>Data Exame8: ".$data8."<br/>";
+?>
